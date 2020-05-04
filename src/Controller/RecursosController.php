9d74bfac4d3cfb7fbf04a4b4ae12e0e8326fea7c
Bruno M Noronha <bruno.developer@pm.me>
2020-05-04 20:53:38 -0300

    Implementando CRUD de recursos

--- src/Controller/RecursosController.php
+++ src/Controller/RecursosController.php
@@ -4,12 +4,13 @@ namespace App\Controller;
 
 use App\Entity\Recurso;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class RecursosController
+class RecursosController extends AbstractController
 {
     /**
      * @var EntityManagerInterface
@@ -19,6 +20,31 @@ class RecursosController
     public function __construct(EntityManagerInterface $entityManager)
     {
         $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @Route("/api/recursos", methods={"GET"})
+     */
+    public function index(): Response
+    {
+        $recursoRepository = $this->getDoctrine()->getRepository(Recurso::class);
+        $recursos = $recursoRepository->findAll();
+
+        return new JsonResponse($recursos);
+    }
+
+    /**
+     * @Route("/api/recursos/{id}", methods={"GET"})
+     */
+    public function show(Request $request): Response
+    {
+        $id = $request->get('id');
+        $recursoRepository = $this->getDoctrine()->getRepository(Recurso::class);
+        $recurso = $recursoRepository->find($id);
+
+        $statusCode = is_null($recurso) ? Response::HTTP_NO_CONTENT : Response::HTTP_OK;
+
+        return new JsonResponse($recurso, $statusCode);
     }
 
     /**
@@ -38,5 +64,40 @@ class RecursosController
         $this->entityManager->flush();
 
         return new JsonResponse($recurso);
+    }
+
+    /**
+     * @Route("/api/recursos/{id}", methods={"PUT"})
+     */
+    public function update(Request $request): Response
+    {
+        $id = $request->get('id');
+
+        $requestBody = $request->getContent();
+        $dataJson = json_decode($requestBody);
+
+        $newRecurso = new Recurso();
+        $newRecurso->title = $dataJson->title;
+        $newRecurso->description = $dataJson->description;
+        $newRecurso->url = $dataJson->url;
+
+        $recursoRepository = $this->getDoctrine()->getRepository(Recurso::class);
+        $recurso = $recursoRepository->find($id);
+        if (is_null($recurso)){
+            return new Response('', Response::HTTP_NOT_FOUND);
+        }
+
+        $recurso->title = $newRecurso->title;
+        $recurso->description = $newRecurso->description;
+        $recurso->url = $newRecurso->url;
+
+        $this->entityManager->flush();
+
+        return new JsonResponse($recurso);
+    }
+
+    public function destroy()
+    {
+        
     }
 }
